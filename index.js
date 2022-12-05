9c4faf50ad170ca7e27d0ab0d9cc465b08ba36a5
kebin20 <ktanzyl@hotmail.com>
2022-12-05 17:53:25 +0900

    Coded hamburger menu function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Dropdown function
 document.addEventListener('click', e => {
     const isDropdownButton = e.target.matches("[data-dropdown-button]")
     if(!isDropdownButton && e.target.closest('[data-dropdown]') != null)
@@ -12,4 +13,17 @@ let currentDropdown
         if (dropdown === currentDropdown) return
         dropdown.classList.remove('active')
     })
+})
+
+// Hamburger Menu Function
+const menuButton = document.getElementById("menu-btn")
+const exitButton = document.getElementById("exit-btn")
+const nav = document.getElementById("nav")
+
+menuButton.addEventListener('click', () => {
+nav.classList.add('open-nav')
+})
+
+exitButton.addEventListener('click', () => {
+nav.classList.remove('open-nav')
 })
