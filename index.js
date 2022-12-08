205a85e8942116b98a167c71fd8b86b8921db5ca
kebin20 <ktanzyl@hotmail.com>
2022-12-08 21:06:02 +0900

    Attempting to get the arrow icons turning

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 // import './style.css'
 
 // Dropdown function (FOR DESKTOP MODE)
-document.addEventListener('click', e => {
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
-    if(!isDropdownButton && e.target.closest('[data-dropdown]') != null)
-    return
-let currentDropdown
-    if(isDropdownButton) {
-        currentDropdown = e.target.closest('[data-dropdown]')
-        currentDropdown.classList.toggle('active')
-    }
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+    currentDropdown.getElementById("arrow-features").src =
+      "./images/icon-arrow-up.svg";
+    currentDropdown.getElementById("arrow-company").src =
+      "./images/icon-arrow-up.svg";
+  }
 
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-        if (dropdown === currentDropdown) return
-        dropdown.classList.remove('active')
-    })
-})
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+});
 
 // Hamburger Menu Function
-const menuButton = document.getElementById("menu-btn")
-const exitButton = document.getElementById("exit-btn")
-const nav = document.getElementById("nav")
+const menuButton = document.getElementById("menu-btn");
+const exitButton = document.getElementById("exit-btn");
+const nav = document.getElementById("nav");
 
-menuButton.addEventListener('click', () => {
-nav.classList.add('open-nav')
-})
+menuButton.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
 
-exitButton.addEventListener('click', () => {
-nav.classList.remove('open-nav')
-})
+exitButton.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
+
+// else {
+//     document.getElementById("arrow-features").src =
+//     "./images/icon-arrow-down.svg";
+//   document.getElementById("arrow-company").src = "./images/icon-arrow-down.svg";
+//   }
