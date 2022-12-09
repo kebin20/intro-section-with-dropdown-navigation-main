7ecf50c513d1277bb93a09059f4e7f52de8086a7
kebin20 <ktanzyl@hotmail.com>
2022-12-09 09:13:26 +0900

    Animation of arrow dropdown completed

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
-// import './style.css'
+import './style.css';
 
 // Dropdown function (FOR DESKTOP MODE)
-document.addEventListener("click", (e) => {
-  const isDropdownButton = e.target.matches("[data-dropdown-button]");
-  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
-  let currentDropdown;
-  if (isDropdownButton) {
-    currentDropdown = e.target.closest("[data-dropdown]");
-    currentDropdown.classList.toggle("active");
+
+const arrowFeatures = document.getElementById('arrow-features');
+const arrowCompany = document.getElementById('arrow-company');
+
+document.addEventListener('click', (e) => {
+  const isDropdownButtonFeatures = e.target.matches(
+    '[data-dropdown-button-features]'
+  );
+  if (
+    !isDropdownButtonFeatures &&
+    e.target.closest('[data-dropdown-features]') != null
+  )
+    return;
+  let currentDropdownFeatures;
+  if (isDropdownButtonFeatures) {
+    currentDropdownFeatures = e.target.closest('[data-dropdown-features]');
+    currentDropdownFeatures.classList.toggle('active');
+    arrowFeatures.classList.toggle('arrow-up');
   }
 
-  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
-    if (dropdown === currentDropdown) return;
-    dropdown.classList.remove("active");
-  });
+  const isDropdownButtonCompany = e.target.matches(
+    '[data-dropdown-button-company]'
+  );
+  if (
+    !isDropdownButtonCompany &&
+    e.target.closest('[data-dropdown-company]') != null
+  )
+    return;
+  let currentDropdownCompany;
+  if (isDropdownButtonCompany) {
+    currentDropdownCompany = e.target.closest('[data-dropdown-company]');
+    currentDropdownCompany.classList.toggle('active');
+    arrowCompany.classList.toggle('arrow-up');
+  }
+
+    document.querySelectorAll('[data-dropdown].active').forEach((dropdown) => {
+      if (dropdown === currentDropdown) return;
+      dropdown.classList.remove('active');
+    });
 });
 
 // Hamburger Menu Function
-const menuButton = document.getElementById("menu-btn");
-const exitButton = document.getElementById("exit-btn");
-const nav = document.getElementById("nav");
+const menuButton = document.getElementById('menu-btn');
+const exitButton = document.getElementById('exit-btn');
+const nav = document.getElementById('nav');
 
-menuButton.addEventListener("click", () => {
-  nav.classList.add("open-nav");
+menuButton.addEventListener('click', () => {
+  nav.classList.add('open-nav');
 });
 
-exitButton.addEventListener("click", () => {
-  nav.classList.remove("open-nav");
+exitButton.addEventListener('click', () => {
+  nav.classList.remove('open-nav');
 });
